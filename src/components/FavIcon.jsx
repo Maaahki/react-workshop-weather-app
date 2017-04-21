ae7424dc44ddd540b67dee29be570c2167819428
Fabian Mueller <fmueller@inovex.de>
2017-04-21 11:50:57 +0200

    Refactor: Keep state in WeatherTile
    Refactor code to make FavIcon a stateless component. Keep state in
    Weather tile and implement communication between Parent and child
    component through props (parent -> child) and callbacks (child ->
    parent).

--- src/components/FavIcon.jsx
+++ src/components/FavIcon.jsx
@@ -1,40 +1,13 @@
 import React from 'react';
 
-class FavIcon extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Initialize state
-    this.state = { faved: false };
-
-    // Bind handler to current instance
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    // Prevent any default actions on this event.
-    // Only the state should be updated.
-    event.preventDefault();
-
-    // Best practice: When new state depends on old state, a function should
-    // be used.
-    // The next line often works, but not under all circumstances ...
-    // this.setState({ faved: !this.state.faved });
-    // ... while it's guaranteed to work if a callback function is used.
-    this.setState((prevState) => ({ faved: !prevState.faved }));
-  }
-
-  render() {
-    // Destruct state variables for easier access
-    const { faved } = this.state;
-
-    return (
-      <button className="weather-tile__favicon" onClick={this.handleClick}>
-        { !faved && '☆' }
-        { faved && '★' }
-      </button>
-    );
-  }
+function FavIcon(props) {
+  const { faved, onClick } = props;
+  return (
+    <button className="weather-tile__favicon" onClick={onClick}>
+      { !faved && '☆' }
+      { faved && '★' }
+    </button>
+  );
 
 }
 
--- src/components/WeatherTile.jsx
+++ src/components/WeatherTile.jsx
@@ -1,18 +1,56 @@
 import React from 'react';
 import FavIcon from './FavIcon';
 
-function WeatherTile(props) {
-  return (
-    <div className="weather-tile weather-tile--rainy">
-      <h1 className="weather-tile__city">{props.locationName}</h1>
-      <div className="weather-tile__temperature">
-        <span className="temperature__min">{props.minTemperature}°</span>
-        <span className="temperature__max">{props.maxTemperature}°</span>
-        <span className="temperature__current">{props.temperature}°</span>
+class WeatherTile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state
+    this.state = { faved: false };
+
+    // Bind handler to current instance
+    this.handleClickFavIcon = this.handleClickFavIcon.bind(this);
+  }
+
+  handleClickFavIcon(event) {
+    // Prevent any default actions on this event.
+    // Only the state should be updated.
+    event.preventDefault();
+
+    // Best practice: When new state depends on old state, a function should
+    // be used.
+    // The next line often works, but not under all circumstances ...
+    // this.setState({ faved: !this.state.faved });
+    // ... while it's guaranteed to work if a callback function is used.
+    this.setState((prevState) => ({ faved: !prevState.faved }));
+  }
+
+  render() {
+    // Destruct state variables for easier access
+    const { faved } = this.state;
+    // Destruct props variables for easier access
+    const {
+      locationName,
+      minTemperature,
+      maxTemperature,
+      temperature
+    } = this.props;
+
+    return (
+      <div className="weather-tile weather-tile--rainy">
+        <h1 className="weather-tile__city">{locationName}</h1>
+        <div className="weather-tile__temperature">
+          <span className="temperature__min">{minTemperature}°</span>
+          <span className="temperature__max">{maxTemperature}°</span>
+          <span className="temperature__current">{temperature}°</span>
+        </div>
+        <FavIcon
+          faved={faved}
+          onClick={this.handleClickFavIcon}
+        />
       </div>
-      <FavIcon />
-    </div>
-  );
+    );
+  }
 }
 
 export default WeatherTile;
