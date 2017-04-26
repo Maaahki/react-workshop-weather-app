0a1cac93ebf87749d4513a8d4f029ca2258a9cfd
Fabian Mueller <fmueller@inovex.de>
2017-04-26 19:06:25 +0200

    Refactor: Improve code by wrapping Provider around App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import './App.css';
-import { Provider } from 'react-redux';
-import  { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import { connect } from 'react-redux';
 
 import SearchBox from './components/SearchBox';
 import WeatherList from './components/WeatherList';
 import ResultInfo from './components/ResultInfo';
-import weatherApp from './reducers';
 import { fetchWeatherData } from './actions';
-
-// Create store that is used as a singleton accross the application.
-// Apply middleware to add further capabilities to the redux store.
-const store = createStore(
-  weatherApp,
-  applyMiddleware(
-    thunk,
-    logger
-  )
-);
+import './App.css';
 
 class App extends React.Component {
 
   componentDidMount() {
     // It's safe to do networking after the component has been mounted,
     // so make a request to the back-end in order to query the data.
-    store
-      .dispatch(fetchWeatherData())    // dispatch action that fetches the weather data
+    this.props.onFetchWeatherData();
   }
 
   render() {
@@ -35,16 +20,29 @@ class App extends React.Component {
     // This provider applies the magic that connects the application with the
     // singleton store.
     return (
-      <Provider store={store}>
-        <main className="weather-app">
-          <SearchBox />
-          <ResultInfo />
-          <WeatherList />
-        </main>
-      </Provider>
+      <main className="weather-app">
+        <SearchBox />
+        <ResultInfo />
+        <WeatherList />
+      </main>
     );
   }
 
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onFetchWeatherData: () => dispatch(fetchWeatherData())
+  };
+}
+
+const ConnectedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default ConnectedApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import { Provider } from 'react-redux';
+import  { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import weatherApp from './reducers';
 import './index.css';
 
+// Create store that is used as a singleton accross the application.
+// Apply middleware to add further capabilities to the redux store.
+const store = createStore(
+  weatherApp,
+  applyMiddleware(
+    thunk,
+    logger
+  )
+);
+
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
